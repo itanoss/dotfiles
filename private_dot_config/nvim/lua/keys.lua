f5e73878dae94d88e7172d853ac1742bc7a410b3
Joonhyeok Im <itanoss@gmail.com>
2023-06-27 17:49:16 -0700

    nvim updated

--- private_dot_config/nvim/lua/keys.lua
+++ private_dot_config/nvim/lua/keys.lua
@@ -2,7 +2,7 @@ local keymap_opts = { buffer = buffer }
 -- Code navigation and shortcuts
 vim.keymap.set("n", "<c-]>", vim.lsp.buf.definition, keymap_opts)
 vim.keymap.set("n", "K", vim.lsp.buf.hover, keymap_opts)
-vim.keymap.set("n", "gD", vim.lsp.buf.implementation, keymap_opts)
+vim.keymap.set("n", "gI", vim.lsp.buf.implementation, keymap_opts)
 vim.keymap.set("n", "<c-k>", vim.lsp.buf.signature_help, keymap_opts)
 vim.keymap.set("n", "1gD", vim.lsp.buf.type_definition, keymap_opts)
 vim.keymap.set("n", "gr", vim.lsp.buf.references, keymap_opts)
@@ -10,7 +10,9 @@ vim.keymap.set("n", "g0", vim.lsp.buf.document_symbol, keymap_opts)
 vim.keymap.set("n", "gW", vim.lsp.buf.workspace_symbol, keymap_opts)
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, keymap_opts)
 vim.keymap.set("n", "<Leader>a", vim.lsp.buf.code_action, keymap_opts)
-vim.keymap.set("x", "<Leader>a", vim.lsp.buf.range_code_action, keymap_opts)
+vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, keymap_opts)
+vim.keymap.set("v", "<F2>", vim.lsp.buf.rename, keymap_opts)
+--vim.keymap.set("x", "<Leader>a", vim.lsp.buf.range_code_action, keymap_opts)
 
 -- refactoring
 -- Remaps for the refactoring operations currently offered by the plugin
